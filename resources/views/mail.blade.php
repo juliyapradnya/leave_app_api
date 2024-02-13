3bf733982fe25a20576d29542f32458ab93457a7
Juliya <70242702+juliyapradnya@users.noreply.github.com>
2024-02-13 10:06:55 +0800

    first commit

---
+++ resources/views/mail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Halo Bapak/Ibu</h2>
+    <p>{{ $detail['body'] }} mau liburan katanya</p>
+</body>
+</html>
